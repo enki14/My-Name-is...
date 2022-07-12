63ab0327ed07b3a08939578405701dc263dd9738
enki14 <jyakarisuto@gmail.com>
2022-07-13 03:18:54 +0900

    20220713 commit

--- index.php
+++ index.php
@@ -11,7 +11,13 @@
     <header>
         <?php wp_nav_menu(array('theme_location'=>'header-navigation')); ?>
         <div class="h_wrap">
-            <h1>求められたものを作りたい<br>ただそれだけです</h1>
+            <h1>
+                <p class="p_1">ようこそ<br>のりまきさん家へ</p>
+                <p class="p_2">このサイトは<br>私のweb制作における作品集<br>および成長日記となります</p>
+                <p class="p_3">ホームページを立ち上げたい<br>代わりに作ってほしい</p>
+                <p class="p_4">などのご依頼がありましたら<br>どうぞご気軽にお問い合わせください</p>
+            </h1>
+            
         </div>
     </header>
     <main>
@@ -88,41 +94,248 @@
             </div>
         </section>
         <section id="about">
-            <div class="about_aria">
-                <div class="about_wrap">
-                    <h3>
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/34/About.png" alt="about">
-                    </h3>
-                    <p class="a_overview">
-                        こちらは私、カリストの砂の案件獲得用ポートフォリオサイトとなっております。
-                        主に、これまでのサイト制作を発信し、また今後も新しい練習素材やお仕事での制作物を可能な限り更新していくものとなります。
-                    </p>
-                    <article>
-                        <div class="about_img"></div>
-                        <ul>
-                            <li>サイト&emsp;&emsp;&emsp;My Name is...</li>
-                            <li>所在地&emsp;&emsp;&emsp;東京都中野区</li>
-                            <li>お問い合わせ&emsp;&emsp;&emsp;jyakarisuto.gmail.com</li>
-                            <li>事業内容&emsp;&emsp;&emsp;コーディング、CMS導入等</li>
-                            <li class="list_space"></li>
-                            <li>Callisto&nbsp;no&nbsp;suna</li>
-                            <li>Twitter&emsp;&emsp;&emsp;@RBtIpvNr6OkIKiX </li>
-                            <li>Facebook&emsp;&emsp;&emsp;https://www.facebook.com/jyakarisuto</li>
-                        </ul>
-
-                    </article>
-                    <div class="profile_aria">
-                        <h4>自己紹介</h4>
-                        <p class="profile">個人的なサイト運営からプログラミングやホームページ作成に興味を持ち、3年の月日を経て今に至ります。
-                        現在は都内の介護施設に勤務。その傍ら専業のフリーランスを目指し
-                        10年20年を見据えて質の良い開発のために日々精進しております。</p>
-                    </div>
+            <div class="about_top">
+                <div class="top_mountain">
                 </div>
             </div>
-            
+            <div class="about_wrap">
+                <h3>
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/34/About.png" alt="about">
+                </h3>
+                <p class="a_overview">
+                    こちらは私、カリストの砂の案件獲得用ポートフォリオサイトとなっております。
+                    主に、これまでのサイト制作を発信し、また今後も新しい練習素材やお仕事での制作物を可能な限り更新していくものとなります。
+                </p>
+                <article>
+                    <div class="about_img"></div>
+                    <ul>
+                        <li>サイト&emsp;&emsp;
+                            <span class='d_line'></span>
+                            &emsp;&emsp;My Name is...
+                        </li>
+                        <li>所在地&emsp;&emsp;
+                            <span class='d_line'></span>
+                            &emsp;&emsp;東京都中野区
+                        </li>
+                        <li>お問い合わせ&emsp;&emsp;
+                            <span class='d_line'></span>
+                            &emsp;&emsp;jyakarisuto.gmail.com
+                        </li>
+                        <li>事業内容&emsp;&emsp;
+                            <span class='d_line'></span>
+                            &emsp;&emsp;コーディング、CMS導入等
+                        </li>
+                        <li class="list_space"></li>
+                        <li>Callisto&nbsp;no&nbsp;suna</li>
+                        <li>Twitter&emsp;&emsp;&emsp;@RBtIpvNr6OkIKiX </li>
+                        <li>Facebook&emsp;&emsp;&emsp;https://www.facebook.com/jyakarisuto</li>
+                    </ul>
+
+                </article>
+                <div class="profile_aria">
+                    <h4>自己紹介</h4>
+                    <p class="profile">個人的なサイト運営からプログラミングやホームページ作成に興味を持ち、3年の月日を経て今に至ります。
+                    現在は都内の介護施設に勤務。その傍ら専業のフリーランスを目指し
+                    10年20年を見据えて質の良い開発のために日々精進しております。</p>
+                </div>
+            </div>
+            <div class="about_bottom">
+                <div class="bottom_mountain">
+                </div>
+            </div>
+        </section>
+        <section id="contact">
+            <h3>
+                <img src="<?php echo get_template_directory_uri(); ?>/images/34/Contact.png" alt="contact">
+            </h3>
+            <?php echo do_shortcode('[contact-form-7 id="50" title="お問い合わせ"]'); ?>
         </section>
     </main>
-    <footer></footer>
+    <footer>
+        <div class="f_wrap">
+            <ul class="sns_aria">
+                <li class="sstw">
+                    <a href="https://twitter.com/share?url=<?php echo get_permalink(); ?>&text=<?php echo get_the_title(); ?>"
+                    target="_blank" rel="nofollow noopener">
+                        <i class="fa-brands fa-twitter fa-lg"></i>
+                    </a>
+                </li>
+                <li class="ssfb">
+                    <a href="http://www.facebook.com/share.php?u=<?php echo get_permalink(); ?>"
+                    target="_blank" rel="nofollow noopener">
+                        <i class="fa-brands fa-facebook-square fa-lg"></i>
+                    </a>
+                </li>
+                <li class="ssins">
+                    <a href="#" title="<?php echo get_the_title(); ?>"
+                    target="_blank" rel="nofollow noopener">
+                        <i class="fa-brands fa-instagram fa-lg"></i>
+                    </a>
+                </li>
+            </ul>
+        <p>copyright, My Name is...</p>
+        </div>
+    </footer>
+    <div class="start">
+        <div class="moon_wrap">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/pc/Ellipse 16.png" alt="月光">
+        </div>
+        <img src="<?php echo get_template_directory_uri(); ?>/images/pc/norimaki.png" 
+        class="norimakisan" alt="のりまきさんのロゴ">
+        <div class="cloudy_area">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/pc/Group 147.png" 
+            class="cloudy" alt="雲その１">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/pc/Group 147.png" 
+            class="cloudy" alt="雲その２">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/pc/Group 147.png" 
+            class="cloudy" alt="雲その３">
+        </div>
+    </div>
+    <script>
+
+        // 帯がにゅーっと出てくる奴
+        jQuery(function(){
+            let blue_band = $('.one_post:nth-of-type(4n+1)');
+            let yellow_band = $('.one_post:nth-of-type(4n+3)');
+            
+            // 疑似要素のスタイルをまるっと指定する
+            let blue_css = `
+                .one_post:nth-of-type(4n+1)::before{
+                    position: absolute;
+                    content: "";
+                    display: inline-block;
+                    background: #6CA5E8 repeat-x;
+                    border-radius: 175px 0 0 175px;
+                    top: 0;
+                    width: 100%;
+                    height: 350px;
+                    z-index: 1;
+                    animation: slideIn 1.5s ease-in-out forwards; 
+                }
+
+                @keyframes slideIn {
+                    0%{
+                        transform: translateX(95vw);
+                        opacity: 1;
+                    }
+                    100%{
+                        transform: translateX(2vw);
+                        opacity: 1;
+                    }
+                }`;
+
+            let style = $('<style>');
+            style.text(blue_css);
+
+            let yellow_css = `
+                .one_post:nth-of-type(4n+3)::before{
+                    position: absolute;
+                    content: "";
+                    display: inline-block;
+                    background: #EFBA30 repeat-x;
+                    border-radius: 0 175px 175px 0;
+                    top: 0;
+                    width: 100%;
+                    height: 350px;
+                    z-index: 1;
+                    animation: slideOut 1.5s ease-in-out forwards; 
+                }
+
+                @keyframes slideOut {
+                    0%{
+                        transform: translateX(-95vw);
+                        opacity: 1;
+                    }
+                    100%{
+                        transform: translateX(-3vw);
+                        opacity: 1;
+                    }
+                }
+            `;
+            let style_2 = $('<style>');
+            style_2.text(yellow_css);
+
+
+            function moveAnime(){
+                blue_band.each(function(){
+                    let blue_top = blue_band.offset().top;
+                    let scroll = $(window).scrollTop();
+                    let windowHeight = $(window).height();
+                    if(scroll >= blue_top - windowHeight){
+                        $('body').append(style);
+                    }
+                });
+
+                yellow_band.each(function(){
+                    let yellow_top = yellow_band.offset().top;
+                    let scroll = $(window).scrollTop();
+                    let windowHeight = $(window).height();
+                    if(scroll >= yellow_top - windowHeight){
+                        $('body').append(style_2);
+                    }
+                });
+
+            }
+
+            $(window).scroll(function(){
+                moveAnime();
+            });
+        });
+
+        jQuery(function(){
+            let a_top = $('.about_top');
+            let a_bottom = $('.about_bottom');
+
+            let t_before = `
+                .about_top::before{
+                    opacity: 1;
+                }
+            `;
+
+            let style_3 = $('<style>');
+            style_3.text(t_before);
+
+
+            let t_after = `
+                .about_top::after{
+                    opacity: 1;
+                }
+            `;
+
+            let style_4 = $('<style>');
+            style_4.text(t_after);
+
+            
+            function deformation(){
+                a_top.each(function(){
+                    let a_topTop = a_top.offset().top;
+                    let scroll = $(window).scrollTop();
+                    let windowHeight = $(window).height();
+                    if(scroll >= a_topTop - windowHeight){
+                       $('body').append(style_3, style_4);
+                    }
+                });
+            };
+
+            $(window).scroll(function(){
+                deformation();
+            });
+
+        });
+
+        // 最初のアニメーション
+        jQuery(function(){
+            $(window).on('load', function(){
+                setTimeout(() => {
+                    $('.start').fadeIn(300);
+                }, 500);
+                setTimeout(() => {
+                    $('.start').fadeOut(500);
+                }, 5000);
+            });
+
+        });
+    </script>
 <?php wp_footer(); ?>
 </body>
 </html>
